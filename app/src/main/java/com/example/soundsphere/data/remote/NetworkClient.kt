abb7ccfa072081ff8438ff5f49fe4ac40dfec12e
NayanPatidar <nayanpatidar28@gmail.com>
2025-05-31 02:20:46 +0530

    NetworkClient.kt Instance

---
+++ app/src/main/java/com/example/soundsphere/data/remote/NetworkClient.kt
@@ -0,0 +1,34 @@
+package com.example.soundsphere.data.remote
+
+import com.example.soundsphere.data.remote.api.MusicApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object NetworkClient {
+    private const val BASE_URL = "https://jiosaavn-api-ts-psi.vercel.app/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val musicApiService: MusicApiService by lazy {
+        retrofit.create(MusicApiService::class.java)
+    }
+}
